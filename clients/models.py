56f5e920ed41a50f3dd6f6dc71d492e362205157
Artem Tabalin <tabalinas@gmail.com>
2015-06-12 23:10:43 +0300

    Views: Add clients app with index view

---
+++ clients/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Client(models.Model):
+    name = models.CharField(max_length=128)
+    age = models.IntegerField()
+    address = models.CharField(max_length=256)
+    married = models.BooleanField()
+
+    def __str__(self):
+        return self.name;
