21643540c2f90604a906395ceb35ab57db2d98a6
Artem Tabalin <tabalinas@gmail.com>
2015-06-13 16:18:29 +0300

    API: Add client create/update/delete opetations

--- clients/urls.py
+++ clients/urls.py
@@ -5,4 +5,5 @@ from .views import Clients
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^api/?$', Clients.as_view()),
+    url(r'^api/(?P<client_id>[0-9]+)/?$', Clients.as_view()),
 ]
--- clients/views.py
+++ clients/views.py
@@ -14,11 +14,32 @@ class Clients(Resource):
 
     def get(self, request):
         clients = Client.objects.all() \
-            .filter(name__contains=request.GET.get('name')) \
-            .filter(address__contains=request.GET.get('address'));
+            .filter(name__contains = request.GET.get('name')) \
+            .filter(address__contains = request.GET.get('address'));
+        return HttpResponse(self.to_json(clients), content_type = 'application/json', status = 200)
 
-        json_serializer = serializers.get_serializer('json')()
-        clientsJSON = json_serializer.serialize(clients)
+    def post(self, request):
+        Client.objects.create(
+            name = request.POST.get("name"),
+            age = request.POST.get("age"),
+            address = request.POST.get("address"),
+            married =  True if request.POST.get("married") == 'true' else False
+        )
+        return HttpResponse(status = 201)
 
-        return HttpResponse(clientsJSON, content_type='application/json', status=200)
+    def put(self, request, client_id):
+        client = Client.objects.get(pk = client_id)
+        client.name = request.PUT.get("name")
+        client.age = request.PUT.get("age")
+        client.address = request.PUT.get("address")
+        client.married = True if request.PUT.get("married") == 'true' else False
+        client.save()
+        return HttpResponse(status = 200)
 
+    def delete(self, request, client_id):
+        client = Client.objects.get(pk = client_id)
+        client.delete()
+        return HttpResponse(status = 200)
+
+    def to_json(self, objects):
+        return serializers.serialize('json', objects)
